9f05ba86a2d3af05a1e8c6d7ee18442af20fcdcd
Elodie Morin <Elodie.morin1298@gmail.com>
2019-05-20 18:19:03 +0200

    app component update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,11 @@ export class MyApp {
   //TODO: Uncomment following line
   // rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, scOr: ScreenOrientation) {
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              scOr: ScreenOrientation,
+              avatar: AvatarProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
