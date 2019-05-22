b23b1a90cfdb8e33f6006146aa7766ed2064a1d9
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-05-22 12:32:24 +0200

    Back and Alert to quit Quiz

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NavController, Platform} from 'ionic-angular';
+import {AlertController, NavController, Platform} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,13 +7,33 @@ import {NavController, Platform} from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public platform: Platform) {
+  constructor(public navCtrl: NavController, public platform: Platform, public alertCtrl: AlertController) {
     platform.registerBackButtonAction(() => {
 
     switch (navCtrl.getActive().component.thisName) {
       case "QuestionReadingPage":
-        break;
       case "QuestionAnswerPage":
+      case "ResultsPage":
+      case "AvatarProgressPage":
+        this.alertCtrl.create({
+          title: 'Quitter le Quiz',
+          message: 'Voulez-vous quitter le quiz ?',
+          buttons: [
+            {
+              text: 'Non',
+              role: 'cancel',
+              handler: () => {
+                console.log('Cancel clicked');
+              }
+            },
+            {
+              text: 'Oui',
+              handler: () => {
+                this.navCtrl.setRoot(HomePage);
+              }
+            }
+          ]
+        });
         break;
       default:
         this.navCtrl.pop()
