4e85991c91bd81b7e4d196dbd5fd278611745f09
Rousseau Romain <romain.rousseau5@gmail.com>
2019-05-22 10:31:43 +0200

    Added explanation to question
    Modified question class
    Modified HTML to display said explanation

--- src/logic/question.ts
+++ src/logic/question.ts
@@ -1,6 +1,6 @@
 export class Question {
 
-  constructor(public label: string, public dummyReps: Array<string>, public good: string) {
+  constructor(public label: string, public dummyReps: Array<string>, public good: string, public explanation: string) {
 
   }
 }
